9c78285db19131f9d2e5cafdaf7dc8f8da394e6a
Angel <antwiangel77@gmail.com>
2025-01-10 01:01:42 -0600

    linked the structure and the script

--- frontend/home_page/pagescript.js
+++ frontend/home_page/pagescript.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('about-button').addEventListener('click', function() {
-      window.location.href = 'about_page/about.html';
+      window.location.href = '../about_page/about.html';
   });
 
   document.getElementById('sign-up-button').addEventListener('click', function() {
-      window.location.href = 'frontend/sign_up page/sign_up.html';
+      window.location.href = '../sign_up page/sign_up.html';
   });
 });
